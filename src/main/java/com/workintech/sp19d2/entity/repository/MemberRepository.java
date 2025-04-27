d89ce07b3951fc7b4df25a4023d5951995e5dc83
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 22:34:09 +0300

    Create MemberRepository.java

---
+++ src/main/java/com/workintech/sp19d2/entity/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.workintech.sp19d2.repository;
+
+import com.workintech.sp19d2.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("SELECT m FROM Member m WHERE m.email=:email")
+    Optional<Member> findByEmail(String email);
+}
