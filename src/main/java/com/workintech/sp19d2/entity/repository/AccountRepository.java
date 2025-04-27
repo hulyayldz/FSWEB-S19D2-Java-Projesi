bdfa47f44d693867cd1cf3944da8f79710f40efe
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 22:35:07 +0300

    Delete src/main/java/com/workintech/sp19d2/entity/repository directory

--- src/main/java/com/workintech/sp19d2/entity/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.workintech.sp19d2.repository;
-
-import com.workintech.sp19d2.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
