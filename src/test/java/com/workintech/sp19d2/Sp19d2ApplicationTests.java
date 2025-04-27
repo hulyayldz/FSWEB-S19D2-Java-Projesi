05d8f7a1954b8014e8ace5a01631aedee711855b
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 22:26:22 +0300

    Create Sp19d2ApplicationTests.java

---
+++ src/test/java/com/workintech/sp19d2/Sp19d2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.sp19d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp19d2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
