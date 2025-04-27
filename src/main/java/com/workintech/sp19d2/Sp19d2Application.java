e9a8f9514607f0dde7e8e4acc804d524fd030b30
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 22:28:04 +0300

    Create Sp19d2Application.java

---
+++ src/main/java/com/workintech/sp19d2/Sp19d2Application.java
@@ -0,0 +1,13 @@
+package com.workintech.sp19d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp19d2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp19d2Application.class, args);
+	}
+
+}
